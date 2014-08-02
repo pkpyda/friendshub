f3943e38c3250207bfeeaf18b04f41ee82895f14
pkpyda <pydapraveenkumar@gmail.com>
2014-08-02 11:06:53 +0100

    generated new application and scaffolded statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendshub::Application.config.secret_token = '59b371025c0b3aafd7d7e8db3ef6ee8da926171f7780a5c1d69eebf9a7dcd08c3ad125b88d71db56628b1d5111321cef2d6ae1b3f3c31af8d9df0e8f55c9d079'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Friendshub::Application.config.session_store :cookie_store, key: '_friendshub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Friendshub::Application.config.session_store :active_record_store
